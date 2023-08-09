1183ba54a2a77da0d6d8d57bc428d82e614d09c7
ArmanNavodia <armannavodia2002@gmail.com>
2023-08-09 23:02:04 +0530

    upto day 7

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./component/Home";
+import Name from "./component/Name";
+import Price from "./component/Price";
+import Form from "./component/Form";
+import { ToastContainer } from "react-toastify";
+// import Register from "./page/Register";
+import Header from "./component/Header";
+import Login from "./page/Login";
+import Footer from "./component/Footer";
 
 function App() {
+  const bookName = "Rich Dad Poor Dad";
+  const bookPrice = 175;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="main">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/book-name" element={<Name name={bookName} />} />
+          <Route path="/book-price" element={<Price price={bookPrice} />} />
+          <Route path="/form" element={<Form />} />
+        </Routes>
+        <Footer />
+        <ToastContainer />
+      </Router>
     </div>
   );
 }
